7afab55751113825d619f22a95c51dad49dae54d
Maximilian Vogt <vogtmh@gmail.com>
2026-02-06 23:13:16 +0700

    bump version to 1.4.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mavodev.batteryanalyzer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.4.4"
+        versionCode = 10
+        versionName = "1.4.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
